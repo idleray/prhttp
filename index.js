50c253ca5e0756b5421f91f71abc53d833ad062a
idleray <idleray@gmail.com>
2022-10-21 12:37:12 +0800

    Bind Vue lifecycle

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import PrHttpClient from './lib/PrHttpClient.js'
 import PrHttpClientBuilder from './lib/PrHttpClientBuilder.js'
 import PrRequest from './lib/PrRequest.js'
 import PrRequestBuilder from './lib/PrRequestBuilder.js'
+import CancelError from './lib/CancelError.js'
 
 export {
     PrHttpClient,
     PrHttpClientBuilder,
     PrRequest,
-    PrRequestBuilder
+    PrRequestBuilder,
+    CancelError
 }
---
+++ lifecycle.js
@@ -0,0 +1,2 @@
+export { default as Lifecycle } from './lib/lifecycle/Lifecycle.js'
+export { default as Vue2lifecycleMixin } from './lib/lifecycle/Vue2LifecycleMixin.js'
